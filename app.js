5a07a726e8394ff0e9de65aa43253637dad7517a
k_r_a_s_h_ <krishna05564@gmail.com>
2018-12-16 16:32:00 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app =express();
+var bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+app.use( express.static( "public" ) );
+
+app.get('/',function(req,res){
+    res.render("index");
+});
+
+app.listen(process.env.PORT||8080,function(){
+    console.log("Server is listening")
+});
